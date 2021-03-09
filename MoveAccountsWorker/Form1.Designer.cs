ea7cc3ceb6ae831e0ade36b0eb8cc501abeb9fa9
Zachary Luedtke <ZacharyL@americollect.com>
2021-03-09 10:24:09 -0600

    Add UI

--- MoveAccountsWorker/Form1.Designer.cs
+++ MoveAccountsWorker/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblRemainingTotalCount = new System.Windows.Forms.Label();
+            this.lblRemainingTotal = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(22, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 31);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "Interest Rate";
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.txtInterestRate.Location = new System.Drawing.Point(209, 85);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(136, 30);
+            this.txtInterestRate.TabIndex = 44;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold);
+            this.txtStatus.Location = new System.Drawing.Point(209, 22);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(136, 30);
+            this.txtStatus.TabIndex = 43;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(22, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 31);
+            this.label2.TabIndex = 42;
+            this.label2.Text = "Status";
+            // 
+            // lblRemainingTotalCount
+            // 
+            this.lblRemainingTotalCount.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRemainingTotalCount.Location = new System.Drawing.Point(237, 262);
+            this.lblRemainingTotalCount.Name = "lblRemainingTotalCount";
+            this.lblRemainingTotalCount.Size = new System.Drawing.Size(133, 31);
+            this.lblRemainingTotalCount.TabIndex = 41;
+            // 
+            // lblRemainingTotal
+            // 
+            this.lblRemainingTotal.AutoSize = true;
+            this.lblRemainingTotal.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold);
+            this.lblRemainingTotal.Location = new System.Drawing.Point(22, 262);
+            this.lblRemainingTotal.Name = "lblRemainingTotal";
+            this.lblRemainingTotal.Size = new System.Drawing.Size(218, 31);
+            this.lblRemainingTotal.TabIndex = 40;
+            this.lblRemainingTotal.Text = "Remaining Total:";
+            // 
+            // btnDone
+            // 
+            this.btnDone.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnDone.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDone.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDone.Location = new System.Drawing.Point(100, 183);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(187, 60);
+            this.btnDone.TabIndex = 39;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblRemainingTotalCount);
+            this.Controls.Add(this.lblRemainingTotal);
+            this.Controls.Add(this.btnDone);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblRemainingTotalCount;
+        private System.Windows.Forms.Label lblRemainingTotal;
+        private System.Windows.Forms.Button btnDone;
     }
 }
 
